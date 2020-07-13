496466f576e81c1e63b5ecf6f41f3c1531a74166
SeeligerN <niklas_seeliger@icloud.com>
2020-07-13 17:05:46 +0200

    expanded ControlLabel with constructor

--- src/ui/ControlLabel.java
+++ src/ui/ControlLabel.java
@@ -4,4 +4,12 @@ import javax.swing.JLabel;
 
 public class ControlLabel extends JLabel {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ControlLabel() {
+		
+	}
 }
