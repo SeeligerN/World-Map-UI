9d918113f47880f76f1d98ad224ecb82b5e6f8d9
SeeligerN <niklas_seeliger@icloud.com>
2020-07-16 21:15:20 +0200

    Added functionality to StatusLabel.java
    Added intended functionality to StatusLabel.java. It is now customizable
    and you can set up listeners to listen for map mode changes.

--- src/ui/controlBar/StatusLabel.java
+++ src/ui/controlBar/StatusLabel.java
@@ -2,6 +2,8 @@ package ui.controlBar;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.GroupLayout;
 import javax.swing.JComboBox;
@@ -21,21 +23,43 @@ public class StatusLabel extends JLabel {
 	private JSeparator sep;
 	private JComboBox<String> mapModeSelector;
 	
-	public StatusLabel() {
+	private String reinforcements;
+	private String bonus;
+	
+	private List<StatusLabelListener> listeners;
+	
+	public StatusLabel(String[] mapModes, String reinforcements, String bonus) {
+		if (reinforcements == null)
+			reinforcements = "";
+		if (bonus == null)
+			bonus = "";
+		this.reinforcements = reinforcements;
+		this.bonus = bonus;
+		
+		if (mapModes == null)
+			mapModes = new String[] {""};
+		
+		listeners = new ArrayList<>();
+		
 		this.setMaximumSize(new Dimension(9999999, 500));
 
 		title = new JLabel("<html><div style='text-align: center;font-size: 16'>"
 				+ "<b>Player Stats</b></div>", SwingConstants.CENTER);
 		title.setFont(new Font("Dialog", Font.PLAIN, 12));
 		
-		status = new JLabel("<html>Reinforcements : 3 <br>Bonus : 2</html>");
+		status = new JLabel("");
 		status.setFont(new Font("Dialog", Font.PLAIN, 12));
+		updateLabel();
 		
 		sep = new JSeparator(SwingConstants.HORIZONTAL);
 		
-		mapModeSelector = new JComboBox<>(new String[] {"geographical", "political"});
+		mapModeSelector = new JComboBox<>(mapModes);
 		mapModeSelector.setMaximumSize(new Dimension(99999999, 20));
 		mapModeSelector.setFont(new Font("Dialog", Font.PLAIN, 12));
+		mapModeSelector.addActionListener(ae -> {
+			for (StatusLabelListener sl : listeners)
+				sl.statusActionPerformed(StatusLabelListener.TYPE_MAP_MODE_CHANGED);
+		});
 		
 		GroupLayout gl = new GroupLayout(this);
 		gl.setAutoCreateContainerGaps(true);
@@ -55,5 +79,40 @@ public class StatusLabel extends JLabel {
 					.addComponent(mapModeSelector)));
 		
 		this.setLayout(gl);
+	}
+	
+	private void updateLabel() {
+		status.setText("<html><head><style>td { padding: 0px; }</style></head><table>"
+				+ "<tr><td>Reinforcements : </td><td><b>" + reinforcements + "</b></td></tr>"
+				+ "<tr><td>Bonus : </td><td><b>" + bonus + "</b></td></tr>"
+				+ "</table></html>");
+	}
+	
+	public void setReinforcements(String reinforcements) {
+		this.reinforcements = reinforcements;
+		updateLabel();
+	}
+	
+	public void setBonus(String bonus) {
+		this.bonus = bonus;
+		updateLabel();
+	}
+	
+	public String getSelectedMapMode() {
+		return (String) mapModeSelector.getSelectedItem();
+	}
+	
+	public void addStatusLabelListener(StatusLabelListener listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeStatusLabelListener(StatusLabelListener listener) {
+		listeners.remove(listener);
+	}
+	
+	public interface StatusLabelListener {
+		public static final int TYPE_MAP_MODE_CHANGED = 0;
+		
+		public void statusActionPerformed(int tpye);
 	}
 }
