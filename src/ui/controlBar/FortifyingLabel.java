43f1489bd636b0b42667cc4a1305922aea9b179c
SeeligerN <niklas_seeliger@icloud.com>
2020-07-16 21:40:57 +0200

    Added functionality to FortifyingLabel
    added inteded functionality to FortifyingLabel.java. It is now
    customizable and you can now set up listeners to listen for changes in
    the slider and a push of the button.

--- src/ui/controlBar/FortifyingLabel.java
+++ src/ui/controlBar/FortifyingLabel.java
@@ -2,6 +2,8 @@ package ui.controlBar;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
@@ -18,42 +20,85 @@ public class FortifyingLabel extends JLabel {
 
 	private JLabel title;
 	private JSlider troopCount;
-	private JButton fortifyButton;
-	
-	String fortifyCountry;
-	
-	FortifyingLabel() {
+	private JButton attackButton;
+
+	private String fortifyingCountry;
+
+	private List<FortifyLabelListener> listeners;
+
+	public FortifyingLabel(String fortifyingCountry, int minTroops, int maxTroops) {
+
+		listeners = new ArrayList<>();
+
+		if (fortifyingCountry == null)
+			fortifyingCountry = "";
+		this.fortifyingCountry = fortifyingCountry;
+
 		this.setMaximumSize(new Dimension(9999999, 500));
-		
-		title = new JLabel("<html><div style='text-align: center;'>"
-				+ "<a style='font-size: 16'><b>Fortify</b></a><br>"
-				+ "fortify <b>Quebec</b>"
-				+ "</div></html>", SwingConstants.CENTER);
+
+		title = new JLabel("", SwingConstants.CENTER);
 		title.setFont(new Font("Dialog", Font.PLAIN, 12));
-		
-		troopCount = new JSlider(1, 4, 1);
+		updateLabel();
+
+		if (maxTroops < minTroops)
+			maxTroops = minTroops;
+
+		troopCount = new JSlider(minTroops, maxTroops, minTroops);
 		troopCount.setMajorTickSpacing(1);
 		troopCount.setPaintLabels(true);
 		troopCount.setOpaque(false);
+		troopCount.addChangeListener(e -> {
+			if (!troopCount.getValueIsAdjusting())
+				for (FortifyLabelListener dl : listeners)
+					dl.fortifyActionPerformed(FortifyLabelListener.TYPE_TROOP_COUNT_CHANGED);
+		});
 
-		fortifyButton = new JButton("Fortify");
-		
+		attackButton = new JButton("Fortify");
+		attackButton.addActionListener(ae -> {
+			for (FortifyLabelListener dl : listeners)
+				dl.fortifyActionPerformed(FortifyLabelListener.TYPE_FORTIFY);
+		});
+
 		GroupLayout gl = new GroupLayout(this);
 		gl.setAutoCreateContainerGaps(true);
 		gl.setAutoCreateGaps(true);
-		
-		gl.setVerticalGroup(gl.createSequentialGroup()
-				.addComponent(title)
-				.addComponent(troopCount)
-				.addGap(0, 50, 9999)
-				.addComponent(fortifyButton));
-		
-		gl.setHorizontalGroup(gl.createSequentialGroup()
-				.addGroup(gl.createParallelGroup()
-					.addComponent(title)
-					.addComponent(troopCount)
-					.addComponent(fortifyButton, GroupLayout.Alignment.TRAILING)));
-		
+
+		gl.setVerticalGroup(gl.createSequentialGroup().addComponent(title).addComponent(troopCount).addGap(0, 50, 9999)
+				.addGroup(gl.createParallelGroup(GroupLayout.Alignment.TRAILING).addComponent(attackButton)));
+
+		gl.setHorizontalGroup(gl.createSequentialGroup().addGroup(gl.createParallelGroup().addComponent(title)
+				.addComponent(troopCount).addComponent(attackButton, GroupLayout.Alignment.TRAILING)));
+
 		this.setLayout(gl);
+	}
+
+	public void setFortifyingCountry(String attackedCountry) {
+		this.fortifyingCountry = attackedCountry;
+	}
+
+	public int getTroopCount() {
+		return troopCount.getValue();
+	}
+
+	public void addFortifyLabelListener(FortifyLabelListener listener) {
+		listeners.add(listener);
+	}
+
+	public void removeFortifyLabelListener(FortifyLabelListener listener) {
+		listeners.remove(listener);
+	}
+
+	private void updateLabel() {
+		title.setText("<html><div style='text-align: center;'>" + "<a style='font-size: 16'><b>Fortify</b></a><br>"
+				+ "fortify <b>" + fortifyingCountry + "</b>" + "</div></html>");
+	}
+
+	public interface FortifyLabelListener {
+
+		public static final int TYPE_FORTIFY = 0;
+		public static final int TYPE_TROOP_COUNT_CHANGED = 2;
+
+		public void fortifyActionPerformed(int type);
+
 	}
 }
