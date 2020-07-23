abfd606b9f411d45db3be52fd6cd65961fa0a006
SeeligerN <niklas_seeliger@icloud.com>
2020-07-23 20:53:36 +0200

    Added documentation to ClickListener

--- src/ui/ClickListener.java
+++ src/ui/ClickListener.java
@@ -1,6 +1,17 @@
 package ui;
 
+/**
+ * This primitive interface is designed to handle Listener Events.
+ * 
+ * @author Niklas S.
+ *
+ */
 public interface ClickListener {
 
+	/**
+	 * This method is to be called when a ClickEvent occurs.
+	 * 
+	 * @param e the ClickEvent housing all relevant information of the click.
+	 */
 	public void handleClick(ClickEvent e);
 }
